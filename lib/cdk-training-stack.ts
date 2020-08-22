3c0e8bf0d503fd935156feb0e1b685acfca74d4c
Waldman200036 <Waldman200036@gmail.com>
2020-08-22 18:17:30 -0500

    Updates for CDK

--- lib/cdk-training-stack.ts
+++ lib/cdk-training-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import * as training_service from '../lib/training_service';
 
 export class CdkTrainingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new training_service.trainingService(this, 'training');
   }
 }
---
+++ lib/training_service.ts
@@ -0,0 +1,34 @@
+import * as core from "@aws-cdk/core";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+export class trainingService extends core.Construct {
+  constructor(scope: core.Construct, id: string) {
+    super(scope, id);
+
+    const handler = new lambda.Function(this, "trainingHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+      code: lambda.Code.asset("resources/DQMCTraining"),
+      handler: "index.main",
+    });
+
+    const api = new apigateway.RestApi(this, "training-api", {
+      restApiName: "training Service",
+      description: "This service serves training."
+    });
+
+    const getTrainingsIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/html": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", getTrainingsIntegration); // GET /
+
+    const training = api.root.addResource("{state}");
+
+    // Get a specific covid19 from bucket with: GET /{state}
+    const getTrainingIntegration = new apigateway.LambdaIntegration(handler);
+
+    training.addMethod("GET", getTrainingIntegration); // GET /{state}
+
+  }
+}
